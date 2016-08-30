6c2f74073cec5bca0feda35fd7006c6e3ef7f905
BrandonDenton <oinkerbob1@gmail.com>
2016-08-29 22:20:03 -0400

    initial commit, just setting up skeleton

---
+++ vigdec.py
@@ -0,0 +1,21 @@
+#!usr/bin/env python
+##################################################################
+# CS483/583 Homework 1 Stuff
+##################################################################
+
+import os
+import socket
+from sys import platform as Platform
+#from msvcrt import getch    # Press any key to exit.
+
+text = []
+found = 0		## flag for whether or not we've guessed the keyspace ##
+
+## Read the ciphertext into a list. ##
+for
+
+## If you build a subset of text roughly equal to the sum of   ##
+##the square of probabilities of occurrences of letters in the ##
+## plaintext message's alphabet, assume your keyspace is of    ##
+##length equal to the size of that subset.                     ##
+while(found == 0):
